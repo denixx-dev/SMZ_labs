b4b9e0432c65debdfda9c17dd1da00f3b5d023ce
denixx-dev <denissm2002@yandex.ru>
2023-11-25 12:31:21 +0300

    some minor changes

--- lab_1/test_lab_1.py
+++ lab_1/test_lab_1.py
@@ -7,58 +7,46 @@ def test_1():
     image = torch.randn(1, 1, 5, 5) 
     kernel = torch.randn(1, 1, 3, 3)
     
-    test1_output1 = torch.from_numpy(conv2D(image[0, 0].numpy(), kernel[0, 0].numpy()))
+    myConv2D = torch.from_numpy(conv2D(image[0, 0].numpy(), kernel[0, 0].numpy()))
 
-    test1_output2 = libConv2d(image, kernel)
+    torchConv2D = libConv2d(image, kernel)
 
-    test1_output1 = test1_output1.to(test1_output2.dtype)
+    myConv2D = myConv2D.to(torchConv2D.dtype)
 
-    assert torch.allclose(test1_output1, test1_output2)
+    assert torch.allclose(myConv2D, torchConv2D)
 
 def test_2():
     image = torch.randn(1, 1, 4, 4) 
     kernel = torch.randn(1, 1, 2, 2) 
     
-    test1_output1 = torch.from_numpy(conv2D(image[0, 0].numpy(), kernel[0, 0].numpy(), stride = 2))
+    myConv2D = torch.from_numpy(conv2D(image[0, 0].numpy(), kernel[0, 0].numpy(), stride = 2))
 
-    test1_output2 = libConv2d(image, kernel, stride = 2)
+    torchConv2D = libConv2d(image, kernel, stride = 2)
 
-    test1_output1 = test1_output1.to(test1_output2.dtype)
+    myConv2D = myConv2D.to(torchConv2D.dtype)
 
-    assert torch.allclose(test1_output1, test1_output2)
-
-def test_3():
-    image = torch.randn(1, 1, 6, 6) 
-    kernel = torch.randn(1, 1, 3, 3) 
-    
-    test1_output1 = torch.from_numpy(conv2D(image[0, 0].numpy(), kernel[0, 0].numpy(), stride = 2))
-
-    test1_output2 = libConv2d(image, kernel, stride = 2)
-
-    test1_output1 = test1_output1.to(test1_output2.dtype)
-
-    assert torch.allclose(test1_output1, test1_output2)
+    assert torch.allclose(myConv2D, torchConv2D)
 
 def test_3():
     image = torch.randn(1, 1, 6, 6) 
     kernel = torch.randn(1, 1, 3, 3) 
     
-    test1_output1 = torch.from_numpy(conv2D(image[0, 0].numpy(), kernel[0, 0].numpy(), stride = 2))
+    myConv2D = torch.from_numpy(conv2D(image[0, 0].numpy(), kernel[0, 0].numpy(), stride = 2))
 
-    test1_output2 = libConv2d(image, kernel, stride = 2)
+    torchConv2D = libConv2d(image, kernel, stride = 2)
 
-    test1_output1 = test1_output1.to(test1_output2.dtype)
+    myConv2D = myConv2D.to(torchConv2D.dtype)
 
-    assert torch.allclose(test1_output1, test1_output2)
+    assert torch.allclose(myConv2D, torchConv2D)
 
 def test_4():
     image = torch.randn(1, 1, 8, 8) 
     kernel = torch.randn(1, 1, 2, 2) 
     
-    test1_output1 = torch.from_numpy(conv2D(image[0, 0].numpy(), kernel[0, 0].numpy(), stride = 2))
+    myConv2D = torch.from_numpy(conv2D(image[0, 0].numpy(), kernel[0, 0].numpy(), stride = 2))
 
-    test1_output2 = libConv2d(image, kernel, stride = 2)
+    torchConv2D = libConv2d(image, kernel, stride = 2)
 
-    test1_output1 = test1_output1.to(test1_output2.dtype)
+    myConv2D = myConv2D.to(torchConv2D.dtype)
 
-    assert torch.allclose(test1_output1, test1_output2)
+    assert torch.allclose(myConv2D, torchConv2D)
